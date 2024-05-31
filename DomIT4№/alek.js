bb0d50b4887e2118fc84d9c665fd0b717f315ac4
alekmiltenov <alekmiltenov@gmail.com>
2024-05-31 06:41:08 +0300

    .

--- DomIT4№/alek.js
+++ DomIT4№/alek.js
@@ -3,14 +3,14 @@ function toggleNightMode() {
 
     if (body.classList.contains("night-mode")) {
         body.classList.remove("night-mode");
-        body.style.backgroundColor = "white";
+        body.style.backgroundColor = "#e5e4e4";
         body.style.color = "black";
         document.getElementById("button").textContent = "Night Mode";
     }   
     
     else {
         body.classList.add("night-mode");
-        body.style.backgroundColor = "black";
+        body.style.backgroundColor = "#212121";
         body.style.color = "white";
         document.getElementById("button").textContent = "Light Mode";
     }
